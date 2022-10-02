41da23a7e5dcdfbea5be71d8cdb2c8b710c77779
Abhishek Dutt <kaushik01232@gmail.com>
2022-10-02 23:31:51 +0530

    Added exceprion handling for project not found while adding prject task  and  while fetching the backlogs

--- ppmtool/ppmtool/src/main/java/com/project/ppmtool/exception/CustomResponseEntityExceptionHandler.java
+++ ppmtool/ppmtool/src/main/java/com/project/ppmtool/exception/CustomResponseEntityExceptionHandler.java
@@ -16,4 +16,13 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
         ProjectIdExceptionResponse exceptionResponse = new ProjectIdExceptionResponse(ex.getMessage());
         return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleProjectNotFoundException(ProjectNotFoundException ex, WebRequest request)
+    {
+        ProjectNotFoundExceptionResponse projectIdExceptionResponse = new ProjectNotFoundExceptionResponse(ex.getMessage());
+        return new ResponseEntity<>(projectIdExceptionResponse,HttpStatus.BAD_REQUEST);
+    }
+
+
 }
--- ppmtool/ppmtool/src/main/java/com/project/ppmtool/services/ProjectTaskService.java
+++ ppmtool/ppmtool/src/main/java/com/project/ppmtool/services/ProjectTaskService.java
@@ -1,9 +1,13 @@
 package com.project.ppmtool.services;
 
 import com.project.ppmtool.domain.Backlog;
+import com.project.ppmtool.domain.Project;
 import com.project.ppmtool.domain.ProjectTask;
+import com.project.ppmtool.exception.ProjectNotFoundException;
 import com.project.ppmtool.repositories.BacklogRepository;
+import com.project.ppmtool.repositories.ProjectRepositoriy;
 import com.project.ppmtool.repositories.ProjectTaskRepository;
+import org.springframework.beans.PropertyAccessException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,38 +19,50 @@ public class ProjectTaskService {
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
-    public ProjectTask addProjectTask(String projectIdentifier,ProjectTask projectTask)
-    {
-        //solving exception project not found
-        //project task(pt) needs tobe added to a specific project , and project cannot be null and Backlog(bl) should exist
-        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
-        //set the bl to pt
-        projectTask.setBacklog(backlog);
-        // we want our project sequence to be like this IDPRO-1....100
-        Integer BacklogSequence = backlog.getPTSequence();
-        //UPDATE THE BL sequence
-        BacklogSequence++;
-        //Add sequence to the project task
-        backlog.setPTSequence(BacklogSequence);
+    @Autowired
+    private ProjectRepositoriy projectRepository;
 
-        projectTask.setProjectSequence(projectIdentifier+"-"+BacklogSequence);
-        projectTask.setProjectIdentifier(projectIdentifier);
-        //INITIAL priority when priority null
-        if(projectTask.getPriority()==null) //In the future we need projectTask.getPriority()== 0 to handle the form
-        {
-            projectTask.setPriority(3);
-    }
-        //INITIAL status when status is null
-        if(projectTask.getStatus()==""|| projectTask.getStatus()==null){
-            projectTask.setStatus("TO_DO");
+    public ProjectTask addProjectTask(String projectIdentifier,ProjectTask projectTask) {
+        try {
+            //solving exception project not found
+            //project task(pt) needs tobe added to a specific project , and project cannot be null and Backlog(bl) should exist
+            Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+            //set the bl to pt
+            projectTask.setBacklog(backlog);
+            // we want our project sequence to be like this IDPRO-1....100
+            Integer BacklogSequence = backlog.getPTSequence();
+            //UPDATE THE BL sequence
+            BacklogSequence++;
+            //Add sequence to the project task
+            backlog.setPTSequence(BacklogSequence);
+
+            projectTask.setProjectSequence(projectIdentifier + "-" + BacklogSequence);
+            projectTask.setProjectIdentifier(projectIdentifier);
+            //INITIAL priority when priority null
+            if (projectTask.getPriority() == null) //In the future we need projectTask.getPriority()== 0 to handle the form
+            {
+                projectTask.setPriority(3);
+            }
+            //INITIAL status when status is null
+            if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+                projectTask.setStatus("TO_DO");
+            }
+
+            return projectTaskRepository.save(projectTask);
+
+        } catch (Exception e) {
+            throw new ProjectNotFoundException("Project Not Found");
+
         }
 
-        return projectTaskRepository.save(projectTask);
-
     }
-
-    public Iterable<ProjectTask> findBacklogById(String backlog_id) {
-        return projectTaskRepository.findByProjectIdentifierOrderByPriority(backlog_id);
+    public Iterable<ProjectTask> findBacklogById(String id) {
+        Project project = projectRepository.findByProjectIdentifier(id);
+        if(project==null)
+        {
+            throw new ProjectNotFoundException("Project with ID : ' "+id+"' does not exist");
+        }
+        return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
     }
 }
 
